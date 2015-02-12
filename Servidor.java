32b0a7b289fb6bcf77311548613d249e0954fcb5
Pablo Varela <pablovarela182@gmail.com>
2015-02-12 17:24:59 +0100

    Ya funcionan los hilos

--- Servidor.java
+++ Servidor.java
@@ -3,31 +3,36 @@ import java.net.*;
 
 public class Servidor implements Runnable {
 
-	ServerSocket s;
-	Socket socket;
+	static ServerSocket s;
+	static Socket socket;
+	static String cadena = "";
 
 	public static void main(String[] args) {
-		(new Thread(new Servidor())).start();
-	}
-
-	public void run() { // Codigo del server
-		String cadena = "";
 
 		try {
 			s = new ServerSocket(1234);
 			socket = new Socket();
 			socket.setReuseAddress(true);
+
 			while (!cadena.equals("stop")) {
 				socket = s.accept();
-				DataInputStream input = new DataInputStream(socket.getInputStream());
-				DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-				cadena = input.readUTF();
-				System.out.println(cadena);
-				output.writeUTF("Hola desde el Servidor!");
+				(new Thread(new Servidor())).start();
 			}
-			socket.close();
 			s.close();
 		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void run() { // Codigo del hilo
+		try {
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			cadena = input.readUTF();
+			System.out.println(cadena);
+			output.writeUTF("Hola desde el Servidor!");
+			socket.close();
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
